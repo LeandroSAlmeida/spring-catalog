4a120a5fba5128ab15b6fc3a89d7c287a56e755f
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-02-14 11:29:01 -0300

    feat: created CategoryController

---
+++ src/main/java/com/springlearning/catalog/controllers/CategoryController.java
@@ -0,0 +1,31 @@
+package com.springlearning.catalog.controllers;
+
+import com.springlearning.catalog.dto.CategoryDTO;
+import com.springlearning.catalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+   @GetMapping
+   public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> list = service.findAll();
+       return ResponseEntity.ok().body(list);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+        CategoryDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+}
