2334ee3c7ef5c608a8233c658bc2846c61a30385
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-02-14 12:47:10 -0300

    feat: created method delete Category, and adding exception

--- src/main/java/com/springlearning/catalog/services/CategoryService.java
+++ src/main/java/com/springlearning/catalog/services/CategoryService.java
@@ -3,10 +3,13 @@ package com.springlearning.catalog.services;
 import com.springlearning.catalog.domain.Category;
 import com.springlearning.catalog.dto.CategoryDTO;
 import com.springlearning.catalog.repositories.CategoryRepository;
+import com.springlearning.catalog.services.exceptions.DatabaseException;
 import com.springlearning.catalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -45,6 +48,18 @@ public class CategoryService {
             return new CategoryDTO(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id não encontrado" + id);
+        }
+    }
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
         }
     }
 }
