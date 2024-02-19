ed6ff00711eb9e65a621d9bfad7aedb53242e577
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-02-19 11:23:23 -0300

    tests: test to delete product

---
+++ src/test/java/com/springlearning/catalog/repositories/ProductRepositoyuTests.java
@@ -0,0 +1,25 @@
+package com.springlearning.catalog.repositories;
+
+import com.springlearning.catalog.domain.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoyuTests {
+
+    @Autowired
+    private ProductRepository repository;
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExist(){
+
+        Long existingId = 1L;
+        repository.deleteById(existingId);
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertFalse(result.isPresent());
+    }
+
+}
