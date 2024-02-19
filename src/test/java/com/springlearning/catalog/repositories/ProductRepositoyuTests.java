49ab9e698407fffa994a6c69d75e37de7361bb11
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-02-19 17:06:42 -0300

    tests: testing whether the product id is incrementing correctly

--- src/test/java/com/springlearning/catalog/repositories/ProductRepositoyuTests.java
+++ src/test/java/com/springlearning/catalog/repositories/ProductRepositoyuTests.java
@@ -1,7 +1,9 @@
 package com.springlearning.catalog.repositories;
 
 import com.springlearning.catalog.domain.Product;
+import com.springlearning.catalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,10 +15,32 @@ public class ProductRepositoyuTests {
 
     @Autowired
     private ProductRepository repository;
+
+    private long existingId;
+    private long countTotalProducts;
+
+
+    @BeforeEach
+    void setUp() throws Exception{
+        Long existingId = 1L;
+        countTotalProducts = 25L;
+    }
+    @Test
+    public void saveSholdPersistWithAutoIncrementWhenIdIsNull(){
+
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = repository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
+
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExist(){
 
-        Long existingId = 1L;
         repository.deleteById(existingId);
         Optional<Product> result = repository.findById(existingId);
         Assertions.assertFalse(result.isPresent());
---
+++ src/test/java/com/springlearning/catalog/tests/Factory.java
@@ -0,0 +1,20 @@
+package com.springlearning.catalog.tests;
+
+import com.springlearning.catalog.domain.Category;
+import com.springlearning.catalog.domain.Product;
+import com.springlearning.catalog.dto.ProductDTO;
+
+import java.time.Instant;
+
+public class Factory {
+    public static Product createProduct(){
+        Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20t03:00:00Z"));
+        product.getCategories().add(new Category(2L, "Electronics"));
+        return product;
+    }
+
+    public static ProductDTO createProductDTO(){
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+}
