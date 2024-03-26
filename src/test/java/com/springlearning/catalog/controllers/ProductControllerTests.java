9b14573ace62f9a454c70716bba56fa6556fdbd7
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-03-26 16:32:54 -0300

    tests: tests methods delete when id existis and no exist, and insert methods

--- src/test/java/com/springlearning/catalog/controllers/ProductControllerTests.java
+++ src/test/java/com/springlearning/catalog/controllers/ProductControllerTests.java
@@ -22,8 +22,7 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -65,6 +64,8 @@ public class ProductControllerTests {
         doThrow(ResourceNotFoundException.class).when(service).delete(nonExtistingId);
         doThrow(DatabaseException.class).when(service).delete(dependentId);
 
+        when(service.insert(any())).thenReturn(productDTO);
+
     }
 
     @Test
@@ -85,6 +86,23 @@ public class ProductControllerTests {
         result.andExpect(jsonPath("$.id").exists());
         result.andExpect(jsonPath("$.name").exists());
         result.andExpect(jsonPath("$.description").exists());
+    }
+
+    @Test
+    public void insertShouldReturnProductDTOCreatedIdExist() throws Exception{
+
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result =
+                mockMvc.perform(post("/products", existingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isCreated());
+        result.andExpect(jsonPath("$.id").exists());
+        result.andExpect(jsonPath("$.name").exists());
+        result.andExpect(jsonPath("$.description").exists());
+
     }
 
     @Test
@@ -115,6 +133,22 @@ public class ProductControllerTests {
                 mockMvc.perform(put("/products/{id}", nonExtistingId)
                         .content(jsonBody)
                         .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isNotFound());
+    }
+    @Test
+    public void deleteShouldReturnNoContentWhenIdExists() throws Exception {
+
+        ResultActions result =
+                mockMvc.perform(delete("/products/{id}", existingId)
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isNoContent());
+    }
+    @Test
+    public void deleteShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
+
+        ResultActions result =
+                mockMvc.perform(delete("/products/{id}", nonExtistingId)
                         .accept(MediaType.APPLICATION_JSON));
         result.andExpect(status().isNotFound());
     }
