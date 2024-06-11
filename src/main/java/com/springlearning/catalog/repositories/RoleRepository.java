651ae7f552ded0851e3d4526832d7ea490bae267
Leandro S Almeida <contact.dev.leandro@gmail.com>
2024-06-11 14:54:16 -0300

    feat: create RoleRepository

---
+++ src/main/java/com/springlearning/catalog/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.springlearning.catalog.repositories;
+
+import com.springlearning.catalog.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
